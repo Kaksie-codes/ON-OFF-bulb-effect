c1a6da59831180752ef110163ebbfa57ad983705
Nsikak Akpan <113060075+Kaksie-codes@users.noreply.github.com>
2022-12-29 13:42:58 +0100

    Second Commit
    I added a div that monitors the state of the switch.

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const btnEl = document.querySelector('.btn');
 const bodyEl  = document.querySelector('body');
-const audioEl = document.querySelector('#audio')
+const audioEl = document.querySelector('#audio');
+const detailEl = document.querySelector('.details')
+
+detailEl.textContent = 'OFF'
 btnEl.addEventListener('click', () => {
     bodyEl.classList.toggle('on');
-    audioEl.play()
+    audioEl.play();
+    console.log(bodyEl.classList.contains('on'))
+})
+
+document.addEventListener('click', () => {
+    if(bodyEl.classList.contains('on')){
+        detailEl.textContent = 'ON';
+    }else{
+        detailEl.textContent = 'OFF';
+    }
 })
